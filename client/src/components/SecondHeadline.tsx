17edf5da99c092fc54e9b33d2af38575921d07f6
Jade Pace Thomson <156130326+pace-thomson@users.noreply.github.com>
2025-06-04 18:11:26 +0000

    Got gamification section done

--- client/src/components/SecondHeadline.tsx
+++ client/src/components/SecondHeadline.tsx
@@ -1,31 +1,20 @@
 import infiltrateImage from "../../../public/images/infiltrate_screenshot.png";
 
-// import { useGSAP } from "@gsap/react";
-// import { ScrollTrigger } from "gsap/all";
-// import gsap from "gsap";
 
-// gsap.registerPlugin(ScrollTrigger);
+const openGamePopup = () => {
+  const width = 800;  
+  const height = 600;
 
+  var left = (screen.width/2)-(width/2);
+  var top = (screen.height/2)-(height/2);
+  window.open(
+    "https://infiltrate.aries1542.dev/", 
+    "_blank", 
+    `width=${width},height=${height},left=${left},top=${top}`
+  );
+};
 
 const SecondHeadline = () => {
-
-
-  // useGSAP(() => {
-  //   gsap.from('#stories', {
-  //     y: 150,
-  //     // repeat: -1,
-  //     scrollTrigger: {
-  //       trigger: '#stories',
-  //       start: 'top bottom',
-  //       end: 'top 70%',
-  //       scrub: true,
-  //     },
-  //     ease: 'power1.inOut',
-
-  //   })
-  // }, []);
-
-  
   
   return (
     <section className="md:-mb-2 mt-6 md:mt-0">
@@ -67,17 +56,22 @@ const SecondHeadline = () => {
           </div>
   
           <div className="md:relative mb-5 md:ml-5">
-            <div className="md:absolute md:top-[5em] w-3/4 md:left-[1em]">
-              <img src={infiltrateImage} className="w-[20em] h-[15em]"></img>
+            <div className="md:absolute md:top-[3em] w-3/4 md:left-[1em]">
+              <button onClick={openGamePopup} >
+                <figure className="mb-2 sm:w-[20em]">
+                  <img src={infiltrateImage} className="sm:w-[20em] sm:h-[15em]"/>
+                  <figcaption className="text-center"> <span className="italic">Infiltrate</span> by Nick De Jong</figcaption>
+                </figure>
+              </button>
     
               <div className="w-5/6">
                 <h2 className="mb-2">
                   <span className="red-line w-[1em] h-[2px]"></span>
-                  <span className="font-bold">Can You Solve It?</span>
+                  <span className="font-bold">Can You Beat It?</span>
                 </h2>
                 <p className="text-secondary text-xs ml-2 w-full sm:w-5/6">
-                  Test your skills with a real challenge
-                  built by Utah Tech{" "}
+                  Click the image above to test your skills with 
+                  a real challenge built by Utah Tech{" "}
                   <span className="font-bold">SE{" "}</span> 
                   and{" "}
                   <span className="font-bold">IT{" "}</span> students.
