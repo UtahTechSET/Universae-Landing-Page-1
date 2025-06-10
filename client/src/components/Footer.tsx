1c16dbc1b6b69857728f818c93a8e2d6ae86c172
Jade Pace Thomson <156130326+pace-thomson@users.noreply.github.com>
2025-06-10 20:59:03 +0000

    Footer responsiveness fixed

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -6,45 +6,76 @@ import xImage from "../../../public/images/X.png";
 
 const Footer = () => {
   return (
-    <footer className="relative bg-[--red] h-[20em] border-b-[3em] border-foreground
-      flex flex-col justify-center items-center text-background gap-6">
-      
-      <h1 className="text-2xl font-semibold">
-        Utah Tech University
-      </h1>
+    <footer >
 
-      <h3 className="flex flex-row gap-3">
-        <span> 225 South University Avenue St. George, UT 84770</span>
+      {/* Small Footer */}
+      <div className="sm:hidden bg-[--red] h-[20em] border-b-[3em] border-foreground
+        flex flex-col justify-center items-center text-background">
+        <h1 className="text-2xl font-semibold">
+          Utah Tech University
+        </h1>
 
-        <div className="bg-white h-full w-[0.1em]"></div>
+        <h3>225 South University Avenue St. George, UT 84770</h3>
+        <h3>+1 (435) 652-7500</h3>
+        <h3> admissions@utahtech.edu </h3>
+        <h3> Student Hours: 7:00am - 5:00pm</h3>
+        <h3> Office Hours: 9:00am - 5:00pm</h3>
 
-        <span>+1 (435) 652-7500</span>   
+        <div className="flex flex-row gap-6 mt-6">
+          <a href="https://facebook.com/utahtechu"> 
+            <img className="h-[2em]" src={facebookImage}></img> 
+          </a>
 
-        <div className="bg-white h-full w-[0.1em]"></div>
-        
-        <span> admissions@utahtech.edu </span>
-      </h3>
+          <a href="https://instagram.com/utahtechu"> 
+            <img className="h-[2em]" src={instagramImage}></img> 
+          </a>
 
-      <h3 className="flex flex-row gap-3">
-        <span> Student Hours: 7:00am - 5:00pm</span>
+          <a href="https://twitter.com/utahtechu"> 
+            <img className="h-[2em]" src={xImage}></img> 
+          </a>
+        </div>
+      </div>
 
-        <div className="bg-white h-full w-[0.1em]"></div>
-
-        <span> Office Hours: 9:00am - 5:00pm</span>
-      </h3>
-
-      <div className="flex flex-row gap-6 mt-6">
-        <a href="https://facebook.com/utahtechu"> 
-          <img className="h-[2em]" src={facebookImage}></img> 
-        </a>
-        
-        <a href="https://instagram.com/utahtechu"> 
-          <img className="h-[2em]" src={instagramImage}></img> 
-        </a>
-        
-        <a href="https://twitter.com/utahtechu"> 
-          <img className="h-[2em]" src={xImage}></img> 
-        </a>
+      {/* Larger Footer */}
+      <div className="relative bg-[--red] h-[20em] border-b-[3em] border-foreground
+          hidden sm:flex flex-col justify-center items-center text-background gap-6">
+        <h1 className="text-2xl font-semibold">
+          Utah Tech University
+        </h1>
+  
+        <h3 className="flex flex-row gap-3">
+          <span> 225 South University Avenue St. George, UT 84770</span>
+  
+          <div className="bg-white h-full w-[0.1em]"></div>
+  
+          <span>+1 (435) 652-7500</span>   
+  
+          <div className="bg-white h-full w-[0.1em]"></div>
+          
+          <span> admissions@utahtech.edu </span>
+        </h3>
+  
+        <h3 className="flex flex-row gap-3">
+          <span> Student Hours: 7:00am - 5:00pm</span>
+  
+          <div className="bg-white h-full w-[0.1em]"></div>
+  
+          <span> Office Hours: 9:00am - 5:00pm</span>
+        </h3>
+  
+        <div className="flex flex-row gap-6 mt-6">
+          <a href="https://facebook.com/utahtechu"> 
+            <img className="h-[2em]" src={facebookImage}></img> 
+          </a>
+          
+          <a href="https://instagram.com/utahtechu"> 
+            <img className="h-[2em]" src={instagramImage}></img> 
+          </a>
+          
+          <a href="https://twitter.com/utahtechu"> 
+            <img className="h-[2em]" src={xImage}></img> 
+          </a>
+        </div>
       </div>
 
       
