628f0d01ef1e716fb43fd1d8bc6569bfe59cfe86
Jade Pace Thomson <156130326+pace-thomson@users.noreply.github.com>
2025-06-12 22:36:50 +0000

    Fixed whychoose to use match new design and fixed some responsiveness

--- client/src/components/WhyChooseUs.tsx
+++ client/src/components/WhyChooseUs.tsx
@@ -12,18 +12,19 @@ const WhyChooseUs = () => {
   }
   
   return (
-    <section className="sm:relative mb-16 container mx-auto px-4">
+    <section className="sm:relative mb-16 px-8 md:px-2 lg:px-8">
       <div className="sm:absolute sm:right-10 sm:top-5 big-red-line w-[15em] h-[3px]"></div>
 
-      <div className="pt-[2em] sm:-ml-8">
+      <div className="pt-[2em]">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="flex flex-col md:flex-row justify-between items-start">
             
-            <div className="md:hidden visible flex items-center justify-center relative w-full">
-              <div className="border mb-8 shadow-lg">
-                <iframe src="https://drive.google.com/file/d/1gZBwvVhIBYubbKeviHbVJH-8ghDNUOmt/preview" 
-                  allow="autoplay" className="w-full h-full" allowFullScreen></iframe>
-              </div>
+            <div className="md:hidden visible flex items-center justify-center relative  w-full">
+              {clickedOnVideo
+                ? <iframe className="w-full h-[10em] max-h-[15em] max-w-[30em]" allow="autoplay" allowFullScreen
+                  src="https://drive.google.com/file/d/1gZBwvVhIBYubbKeviHbVJH-8ghDNUOmt/preview?start=1"></iframe>
+                : <img src={videoImage} onClick={handleClick} className="w-full h-full max-w-[30em]"></img>
+              }
             </div>
 
             <span className="red-line w-[5em] mt-[1.2em] h-[2.5px]"></span>
@@ -35,13 +36,15 @@ const WhyChooseUs = () => {
               </h3>
 
               <ul className="listwithcheckmark ml-4">
-                <li className="mb-5 text-sm">Career-ready skills in software engineering, cybersecurity,
+                <li className="md:pl-2 mb-5 text-sm">Career-ready skills in software engineering, cybersecurity,
                 DevOps, and app development.</li>
-                <li className="mb-5 text-sm">Flexible HyFlex learning — online, in person or both.</li>
-                <li className="mb-5 text-sm">Hands-on experience through internships and real-world projects.</li>
-                <li className="mb-5 text-sm">Merit-based scholarships available for qualifying transfer students.</li>
-                <li className="mb-5 text-sm">Seamless credit transfer from Universae to Utah Tech degree programs.</li>
+                <li className="md:pl-2 mb-5 text-sm">Flexible HyFlex learning — online, in person or both.</li>
+                <li className="md:pl-2 mb-5 text-sm">Hands-on experience through internships and real-world projects.</li>
+                <li className="md:pl-2 mb-5 text-sm">Merit-based scholarships available for qualifying transfer students.</li>
+                <li className="md:pl-2 mb-5 text-sm">Seamless credit transfer from Universae to Utah Tech degree programs.</li>
               </ul>
+
+              <div className="big-red-line ml-3 sm:mt-8 w-[90%] h-[3px]"></div>
          
   
               <Button className="rounded-none text-background bg-primary font-light px-8 py-2 
@@ -57,21 +60,20 @@ const WhyChooseUs = () => {
           </div>
 
 
-          <div className="hidden md:visible  md:flex  md:items-center  md:justify-center">
+          <div className="hidden md:visible  md:flex  md:items-center  md:justify-center w-[95%]">
             <div className="relative w-full h-80 max-w-[28em]">
-              <div className="absolute top-0 left-5 w-full h-[17.5em] bg-primary"></div>
+              <div className="absolute top-6 lg:top-12 left-4 w-[27em] h-[15em] lg:w-[28em] lg:h-[17.5em] bg-primary"></div>
 
               {/* New Section with thumbnail */}
-              <div className="absolute top-6 -left-5 w-full h-[17.5em] bg-muted  shadow-lg">
+              <div className="absolute top-2 lg:top-8 w-[27em] h-[15em] lg:w-[28em] lg:h-[17.5em] bg-muted  shadow-lg">
                 {clickedOnVideo
                   ? <iframe className="w-full h-full" allow="autoplay" allowFullScreen
                     src="https://drive.google.com/file/d/1gZBwvVhIBYubbKeviHbVJH-8ghDNUOmt/preview?start=1"></iframe>
-                  : <img src={videoImage} onClick={handleClick} className="w-full h-full object-cover"></img>
+                  : <img src={videoImage} onClick={handleClick} className="w-full h-full"></img>
                 }
               </div>
             </div>
           </div>
-          <div className="big-red-line ml-3 sm:mt-8 w-[90%] h-[3px]"></div>
         </div>
       </div>
     </section>
