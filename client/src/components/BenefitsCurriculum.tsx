1e1803097dc51b236688f1ea573a1f09ae0cc3d8
Jade Pace Thomson <156130326+pace-thomson@users.noreply.github.com>
2025-06-13 16:27:10 +0000

    Got rid of unneeded images for their image links

--- client/src/components/BenefitsCurriculum.tsx
+++ client/src/components/BenefitsCurriculum.tsx
@@ -1,7 +1,3 @@
-import benefitsImage from "../../../public/images/benefits.jpg";
-import curriculumImage from "../../../public/images/curriculum.jpg";
-
-
 const BenefitsCurriculum = () => {
   return (
     <section className="pt-6">
@@ -12,7 +8,8 @@ const BenefitsCurriculum = () => {
           <div className="flex flex-col md:flex-row justify-between items-start 
             w-full bg-[#6F6E6E] p-10 lg:py-[5em]">
 
-            <img className="sm:hidden mb-5" src={benefitsImage}></img>
+              {/* Only appears on the samller screen size */}
+            <img className="sm:hidden mb-5" src="https://photos.smugmug.com/Academics/General/2425/Students-on-Campus/i-rqQx96d/0/K79tpNXh2VqfKD7q2RQDnpGr4vKNmDDV2rGF6VQDB/L/_41A8230-L.jpg"></img>
             
             <span className="red-line h-[2px] bg-red w-[8em] mt-[16px]"></span>
             <div>
@@ -34,22 +31,22 @@ const BenefitsCurriculum = () => {
             </div>
           </div>
 
-              {/* Image div */}
+              {/* Image div with offset background rectangle thing */}
           <div className="hidden sm:block w-full sm:ml-4 lg:ml-[4em] sm:relative">
             <div className="
               bg-[#6F6E6E]
               sm:absolute sm:top-[3.5em] sm:left-8 sm:w-[17em] sm:h-[11em] 
               md:w-[20em] md:h-[13em] md:top-9 md:left-10
-              lg:w-[25em] lg:h-[17em] lg:top-7 lg:left-[4em] "
+              lg:w-[28em] lg:h-[19em] lg:top-7 lg:left-[2em] "
               >
             </div>
             <img className=" 
               bg-[#6F6E6E] 
               sm:w-[17em] sm:absolute sm:top-10 sm:left-5 
               md:w-[20em] md:top-5
-              lg:w-[25em] md:left-7
-              lg:top-4 lg:left-[3em]" 
-              src={benefitsImage}>
+              lg:w-[28em] md:left-7
+              lg:top-4 lg:left-[1em]" 
+              src="https://photos.smugmug.com/Academics/General/2425/Students-on-Campus/i-rqQx96d/0/K79tpNXh2VqfKD7q2RQDnpGr4vKNmDDV2rGF6VQDB/L/_41A8230-L.jpg">
             </img>
           </div>
         </div>
@@ -58,12 +55,13 @@ const BenefitsCurriculum = () => {
         {/* Curriculum Section */}
         <div className="
           pt-8 pb-4 bg-muted grid grid-cols-1 
-          md:pb-[5em] sm:grid-cols-2 md::gap-[3em]">
+          md:pb-[5em] sm:grid-cols-2 md::gap-[3em]"
+        >
 
               {/* Image div */}
           <div className="flex justify-center h-full px-10
             md:pl-8 md:flex-row md:justify-end sm:pr-0 s">
-            <img className="sm:h-[13.5em] md:h-[78%] lg:h-[85%] md:mr-5" src={curriculumImage}></img>
+            <img className="sm:h-[13.5em] md:h-[78%] lg:h-[85%] md:mr-5" src="https://photos.smugmug.com/Academics/General/2122/Non-Traditional-Students/i-pDhD6tb/0/MZwsvCpRVznGCCdNBksPntLXqQhZGvxrZVX48F3cF/L/3U2A8586-L.jpg"></img>
           </div>
           
           <div className="flex flex-col md:flex-row ml-12 sm:ml-4 md:pl-4 md:pt-4 lg:pl-[3em] lg:pt-10">
